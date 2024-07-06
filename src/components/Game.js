337964693441a8d1c5325da360abc5c4090897e9
yusaku-iwamoto <yusaku.iwamoto@gmail.com>
2024-07-06 23:29:44 +0900

    Initial commit

---
+++ src/components/Game.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Board from './Board';
+
+function Game() {
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board />
+            </div>
+        </div>
+    );
+}
+
+export default Game;
